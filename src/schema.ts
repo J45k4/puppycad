24f88d8bffa70f7fd9dd7e45589164b40412164f
Puppy <pusanteemu@gmail.com>
2026-04-16 12:06:00 +0300

    improve schema

--- src/schema.ts
+++ src/schema.ts
@@ -1,25 +1,27 @@
 import type { Point2D } from "./types"
 
 export type Line = {
+	id: string
 	type: "line"
 	p0: Point2D
 	p1: Point2D
 }
 
 export type CornerRectangle = {
+	id: string
 	p0: Point2D
 	p1: Point2D
 }
 
 export type Loop = {
 	id: string
-	points: []
+	vertexIndices: number[]
 }
 
 export type Profile = {
-	type: "profile"
-	vertices: Point2D[]
-	loops: number[][]
+	id: string
+	outerLoopId: string
+	holeLoopIds: string[]
 }
 
 export type DepthExtrude = {
@@ -31,16 +33,38 @@ export type LoopExtrudeTarget = {
 	
 }
 
+export type ProfileReference = {
+	type: "profileRef"
+	sketchId: string
+	profileId: string
+}
+
 export type SolidExtrude = {
 	type: "extrude"
-	target: 
+	id: string
+	name?: string
+	target: ProfileReference
+	depth: number
 }
+
+export type SketchTarget =
+	| {
+			type: "plane"
+			plane: "XY" | "YZ" | "XZ"
+	  }
+	| {
+			type: "face"
+			faceId: string
+	  }
 
 export type Sketch = {
 	type: "sketch"
 	id: string
 	name?: string
 	dirty: boolean
+	target: SketchTarget
 	entities: (Line | CornerRectangle)[]
-	profiles: Profile
+	vertices: Point2D[]
+	loops: Loop[]
+	profiles: Profile[]
 }
