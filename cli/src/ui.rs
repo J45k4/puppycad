0a6d4975c2a147cb1eef986a84c088b0129ef380
Puppy <pusanteemu@gmail.com>
2026-05-04 06:49:43 +0300

    remove rust code from the project

--- cli/src/ui.rs
+++
@@ -1,8 +0,0 @@
-use std::process::ExitCode;
-
-use crate::args::UiArgs;
-use crate::project_items::run_project_items_ui;
-
-pub fn run_ui(args: UiArgs) -> ExitCode {
-    run_project_items_ui(args.inspect)
-}
--- daemon/src/main.rs
+++
@@ -1,7 +0,0 @@
-mod api;
-mod args;
-
-fn main() -> std::process::ExitCode {
-    let args = args::parse_args();
-    api::run_api(args)
-}
